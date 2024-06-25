13348462214fc5771b4a1a1793921c028b6e2097
Khanh Duy <lukiluna23803@gmail.com>
2024-06-25 09:30:09 +0700

    Initial commit

---
+++ WEB1043/Lab1/add.js
@@ -0,0 +1,9 @@
+let a = 10;
+// Set a value of 10
+console.log(a);
+// alert(a);
+
+// Nhập dữ liệu
+var x = prompt("Input n: ");
+//Xuất dữ liệu ra màn hình
+document.write("You entered: " + x);
---
+++ WEB1043/Lab1/demo.js
@@ -0,0 +1,32 @@
+// Kiểu dữ liệu
+// Primitive: chuỗi, số, boolean, null, underfined,..
+// Object: {key : value }
+// Khai báo biến
+// let: phạm vi hoạt động trong block
+// var: phạm vi hoạt động trong function
+// const
+let a = 10;
+let b = "Hello";
+let c = true;
+let d = {
+    name: "Văn A",
+    age: 20
+}
+console.log(typeof(d));
+// alert("Hello\nGoodbye"); -> thông báo trả về true
+confirm('Bạn có chắc muốn xóa không')
+// Thông báo trả về true / false
+const PI=3.14;
+// PI = 3; -> cont: Khai báo là hằng số không được thay đổi giá trị của biến
+console.log(PI);
+
+y = 5;
+console.log("y = " + y)
+// let y = 6;
+// var y = 6;
+console.log("y = " + y)
+
+s = "Nguyễn Khánh Duy"
+console.log("Họ và tên: " + s);
+
+console.log("Hello World");
